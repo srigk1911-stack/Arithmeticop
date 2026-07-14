ad294a74a8c49ad4d1b76c2f87b0701f26bd0a8a
SRIPADHMAPRIYA G <srigk1911@gmail.com>
2026-07-14 11:22:33 +0530

    Added arithmetic program

---
+++ arithemetic.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main()
+{
+ int num1,num2;
+ printf("Enter the value of num1");
+ scanf("%d",&num1);
+ printf("Enter the value of num2");
+ scanf("%d",&num2);
+ int add=num1+num2;
+ printf("%d\n",add);
+ int sub=num1-num2;
+ printf("%d\n",sub);
+ int pro=num1*num2;
+ printf("%d\n",pro);
+ int div=num1/num2;
+ printf("%d\n",div);
+ int mod=num1%num2;
+ printf("%d\n",mod);
+ 
+ return 0;
+}
